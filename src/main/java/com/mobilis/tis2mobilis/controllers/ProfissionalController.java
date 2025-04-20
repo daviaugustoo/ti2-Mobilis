2a3a4239db24fc42375d8f0722345d24f48738b2
lucasfelipeb <lucasfelipe96@hotmail.com>
2025-04-20 20:03:57 -0300

    Sugestão de melhoria 3: melhoria na semântica do status de resposta da requisição

--- src/main/java/com/mobilis/tis2mobilis/controllers/ProfissionalController.java
+++ src/main/java/com/mobilis/tis2mobilis/controllers/ProfissionalController.java
@@ -45,14 +45,18 @@ public class ProfissionalController {
     O @RequestBody é desnecessário e incorreto.
     Para @PathVariable não se usa @RequestBody.
     * */
-    @GetMapping("/id/{id}") //CRIADA
-    public ResponseEntity<Optional<Profissional>> getProfessionalById(@PathVariable Long id) {
-        Optional<Profissional> professional = profissionalService.getProfessionalById(id);
-        if (professional.isPresent()) {
-            return ResponseEntity.status(HttpStatus.FOUND).body(professional);
-        }
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+    /*
+    Sugestão de melhoria 3
+    A resposta HttpStatus.FOUND (302) é semanticamente incorreta para GETs bem sucedidos
+    O certo é HttpStatus.OK.
+    */
+    @GetMapping("/id/{id}")
+    public ResponseEntity<Profissional> getProfessionalById(@PathVariable Long id) {
+        return profissionalService.getProfessionalById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
+
 
     @PutMapping("/{id}")
     public ResponseEntity<Profissional> atualizarProfissional(@PathVariable Long id, @RequestBody Profissional profissionalAtualizado) {
